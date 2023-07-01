d32220217e990e5735608f8001ee17bb7f9a995f
Trung Minh <dangtrungminh456@gmail.com>
2023-07-01 21:40:23 +0700

    Cap nhat them bang vao Database

--- app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -8,7 +8,7 @@ import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "doanandroid.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
     public DatabaseHelper(Context context){
         super(context , DATABASE_NAME, null, DATABASE_VERSION);
     }
