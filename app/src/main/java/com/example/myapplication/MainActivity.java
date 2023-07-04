4d618fd4dcef22f89dd7091690f7f2e33496d12b
Trung Minh <dangtrungminh456@gmail.com>
2023-07-04 21:10:01 +0700

    Xử lý xác thực đăng nhập và cập nhật giao diện form activity_main

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseHelper databaseHelper;
@@ -21,18 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        Button button_login = findViewById(R.id.button_login);
-        button_login.setOnClickListener(new View.OnClickListener() {
+        Button button_login_home = findViewById(R.id.button_login_home);
+
+        button_login_home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Start the Login activity
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
+
+
         });
 
-        Button button_signup = findViewById(R.id.button_signup);
-        button_signup.setOnClickListener(new View.OnClickListener() {
+        Button button_signup_home = findViewById(R.id.button_signup_home);
+        button_signup_home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Start the Login activity
