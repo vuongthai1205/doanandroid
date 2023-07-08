e386141718f39254ba4c83ba3b74737c08e8d020
Trung Minh <dangtrungminh456@gmail.com>
2023-07-08 23:32:06 +0700

    Cập nhật chức năng đặt lại mật khẩu

--- app/src/main/java/com/example/myapplication/view/ResetPasswordActivity.java
+++ app/src/main/java/com/example/myapplication/view/ResetPasswordActivity.java
@@ -1,6 +1,8 @@
-package com.example.myapplication;
+package com.example.myapplication.view;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -16,6 +18,9 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+import com.example.myapplication.config.DatabaseHelper;
 
 public class ResetPasswordActivity extends AppCompatActivity {
     @Override
@@ -39,12 +44,7 @@ public class ResetPasswordActivity extends AppCompatActivity {
             } else if (reset_again_pass.isEmpty()) {
                 edt_reset_again_password.setError("Vui lòng nhập dữ liệu");
             }
-            if (UpdatePassword(email, reset_pass, reset_again_pass)) {
-                Intent intent = new Intent(ResetPasswordActivity.this, LoginActivity.class);
-                startActivity(intent);
-                // Kết thúc Activity hiện tại
-                finish();
-            }
+            UpdatePassword(email, reset_pass, reset_again_pass);
         });
 
 
@@ -106,25 +106,36 @@ public class ResetPasswordActivity extends AppCompatActivity {
     }
 
     //Phương thức updatePassword
-    private boolean UpdatePassword(String email, String resetPassword, String resetAgainPassword) {
+    private void UpdatePassword(String email, String resetPassword, String resetAgainPassword) {
         DatabaseHelper dbHelper = new DatabaseHelper(this);
         SQLiteDatabase database = dbHelper.getWritableDatabase();
-        String[] information = {dbHelper.COLUMN_EMAIL, dbHelper.COLUMN_MAT_KHAU};
-        String select_ten_nguoi_dung = dbHelper.COLUMN_EMAIL + " = ?";
+        String[] information = {dbHelper.getCOLUMN_EMAIL(), dbHelper.getCOLUMN_MAT_KHAU()};
+        String select_ten_nguoi_dung = dbHelper.getCOLUMN_EMAIL() + " = ?";
         String[] select_ten_args = {email};
         Cursor cursor;
-        cursor = database.query(dbHelper.TABLE_THANHVIEN, information, select_ten_nguoi_dung, select_ten_args, null, null, null);
+        cursor = database.query(dbHelper.getTABLE_THANHVIEN(), information, select_ten_nguoi_dung, select_ten_args, null, null, null);
         if (cursor.moveToFirst()) {
-            String select_email = cursor.getString(cursor.getColumnIndexOrThrow(dbHelper.COLUMN_EMAIL));
+            String select_email = cursor.getString(cursor.getColumnIndexOrThrow(dbHelper.getCOLUMN_EMAIL()));
             if (email.equals(select_email) && resetPassword.equals(resetAgainPassword)) {
                 ContentValues values = new ContentValues();
-                values.put(dbHelper.COLUMN_MAT_KHAU, resetPassword);
-                String whereEmail = dbHelper.COLUMN_EMAIL + "=?";
+                values.put(dbHelper.getCOLUMN_MAT_KHAU(), resetPassword);
+                String whereEmail = dbHelper.getCOLUMN_EMAIL() + "=?";
                 String[] wherEmailArgs = {email};
-                int rowSuccses = database.update(dbHelper.TABLE_THANHVIEN, values, whereEmail, wherEmailArgs);
+                int rowSuccses = database.update(dbHelper.getTABLE_THANHVIEN(), values, whereEmail, wherEmailArgs);
                 if (rowSuccses == 1){
                     Toast.makeText(getApplicationContext(), "Đặt lại mật khẩu thành công", Toast.LENGTH_SHORT).show();
-                    return true;
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ResetPasswordActivity.this);
+                    builder.setTitle("Thông báo");
+                    builder.setMessage("Bạn cần đăng nhập lại");
+                    builder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Chuyển hướng đến trang đăng ký tài khoản
+                            Intent intent = new Intent(ResetPasswordActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        }
+                    }).setNegativeButton("Hủy", null);
+                    builder.show();
                 }
                 else
                     Toast.makeText(getApplicationContext(), "Đặt lại mật khẩu không thành công", Toast.LENGTH_SHORT).show();
@@ -134,6 +145,5 @@ public class ResetPasswordActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Kiểm tra lại email", Toast.LENGTH_SHORT).show();
         cursor.close();
         database.close();
-        return false;
     }
 }
