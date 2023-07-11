1cab90bd347f0c4c45ab51430718b408928d5bf7
vuongthai1205 <2051010367vuong@ou.edu.vn>
2023-07-11 11:45:59 +0700

    chuc nang sign up kha chuan ròi

--- app/src/main/java/com/example/myapplication/view/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/view/SignupActivity.java
@@ -48,7 +48,75 @@ public class SignupActivity extends AppCompatActivity {
 
             }
         });
+        handleDeleteText();
+        handleShowPassword();
     }
+
+    private void handleShowPassword() {
+        activitySignupBinding.imgShowPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPass(activitySignupBinding.txtPassword);
+            }
+        });
+        activitySignupBinding.imgShowPasswordAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPass(activitySignupBinding.txtPasswordAgain);
+            }
+        });
+    }
+
+    private boolean showPass(EditText editText) {
+        if (editText.getTransformationMethod() instanceof PasswordTransformationMethod) {
+            // Nếu đang ẩn mật khẩu, hiển thị văn bản
+            editText.setTransformationMethod(null);
+            return true;
+        } else {
+            // Nếu đang hiển thị văn bản, ẩn mật khẩu
+            editText.setTransformationMethod(new PasswordTransformationMethod());
+            return false;
+        }
+    }
+
+
+
+    private void handleDeleteText(){
+        activitySignupBinding.imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteText(activitySignupBinding.txtUserName);
+            }
+        });
+        activitySignupBinding.deleteName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteText(activitySignupBinding.txtName);
+            }
+        });
+        activitySignupBinding.deleteLastName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteText(activitySignupBinding.txtLastName);
+            }
+        });
+        activitySignupBinding.deleteNumberPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteText(activitySignupBinding.txtPhoneNumber);
+            }
+        });
+        activitySignupBinding.deleteEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteText(activitySignupBinding.txtEmail);
+            }
+        });
+    }
+    private void deleteText(EditText view){
+        view.setText("");
+    }
+
 
 }
 
