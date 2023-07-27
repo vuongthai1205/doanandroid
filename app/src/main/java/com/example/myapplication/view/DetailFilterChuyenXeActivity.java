bc1a7c95abac5679fb7d6f9f1318afb2261867c6
Trung Minh <dangtrungminh456@gmail.com>
2023-07-27 11:16:09 +0700

    cap nhat view Filter chuyen xe

---
+++ app/src/main/java/com/example/myapplication/view/DetailFilterChuyenXeActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailFilterChuyenXeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
